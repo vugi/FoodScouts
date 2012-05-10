3d6656359942d9bface3749f3cc54b4d399ac048
Antti Vuorela <antti.vuorela@iki.fi>
2012-05-10 12:43:16 +0300

    Added search page with filtering

--- app.js
+++ app.js
@@ -1,16 +1,40 @@
 var apiUrl = "http://foodscouts.dy.fi/";
+var itemData = [];
 
-$( document ).delegate("#page1", "pageinit", function() {
-	console.log('page1 pageinit');
+$(document).ready(function(){
+	console.log('document ready');
 	
+	var activePageId = $.mobile.activePage.prop("id");
+	console.log("activePageId: "+activePageId);
+	
+	console.log('Loading data');
 	$.getJSON(apiUrl,function(data){
 		console.log(data);
+		itemData = data;
 		$(data).each(function(i,item){
 			var name = item.fields.name;
 			var description = item.fields.description;
 			$("#exploreList").append("<li><h4>"+name+"</h4><p>"+description+"</p></li>");
+			$("#searchList").append("<li><h4>"+name+"</h4><p>"+description+"</p></li>");
 		});
-		$("#exploreList").listview('refresh');
-	})
+		
+		if(activePageId=="searchPage"){
+			$("#searchList").listview('refresh');
+		} else if (activePageId = "explorePage") {
+			$("#exploreList").listview('refresh');
+		}
+	});
+	
+})
+
+$( document ).delegate("#explorePage", "pageinit", function() {
+	console.log('explorePage pageinit');
+	$("#exploreList").listview('refresh');
+	
+});
+
+$( document ).delegate("#searchPage", "pageinit", function() {
+	console.log('searchPage pageinit');
+	$("#searchList").listview('refresh');
 	
 });
