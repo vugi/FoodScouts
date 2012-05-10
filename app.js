1c3ce058ab28a508c8fdb273096b7b65beb91df9
Antti Vuorela <antti.vuorela@iki.fi>
2012-05-10 15:05:42 +0300

    Random recommendations

--- app.js
+++ app.js
@@ -14,24 +14,43 @@ $(document).ready(function(){
 		$(data).each(function(i,item){
 			var name = item.fields.name;
 			var description = item.fields.description;
-			$("#exploreList").append("<li><h4>"+name+"</h4><p>"+description+"</p></li>");
+			//$("#exploreList").append("<li><h4>"+name+"</h4><p>"+description+"</p></li>");
 			$("#searchList").append("<li><h4>"+name+"</h4><p>"+description+"</p></li>");
 		});
 		
 		if(activePageId=="searchPage"){
 			$("#searchList").listview('refresh');
 		} else if (activePageId = "explorePage") {
-			$("#exploreList").listview('refresh');
+			//$("#exploreList").listview('refresh');
+			showRandomRecommendation();
 		}
 	});
 	
+	// Attach event handlers
+	$("#nextRecommendation").click(showRandomRecommendation);
+	
 })
 
-$( document ).delegate("#explorePage", "pageinit", function() {
+$( document ).delegate("#explorePage", "pageshow", function() {
 	console.log('explorePage pageinit');
-	$("#exploreList").listview('refresh');
+	//$("#exploreList").listview('refresh');
+	showRandomRecommendation();
 	
 });
+
+function showRandomRecommendation(){
+	var random = Math.floor(Math.random()*itemData.length);
+	showRecommendation(random);
+}
+
+function showRecommendation(i){
+	var item = itemData[i];
+	if (item){
+		$("#exploreItem").html("<h3>"+item.fields.name+"</h3><p>"+item.fields.description+"</p>");
+	}
+	
+	
+}
 
 $( document ).delegate("#searchPage", "pageinit", function() {
 	console.log('searchPage pageinit');
